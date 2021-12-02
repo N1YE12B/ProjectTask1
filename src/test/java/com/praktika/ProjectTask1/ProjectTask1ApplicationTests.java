0f807d105fb33e20ad04602615276a02837f1009
N1YE12B <pochtafordnevnik@gmail.com>
2021-12-02 12:56:29 +0300

    Initial commit

---
+++ src/test/java/com/praktika/ProjectTask1/ProjectTask1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praktika.ProjectTask1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTask1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
